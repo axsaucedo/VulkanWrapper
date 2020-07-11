5523f2fafe9ec2dd1559f46e2e90106cc70c3592
Alejandro Saucedo <axsauze@gmail.com>
2020-07-11 17:24:24 +0100

    Testing that vulkan works as expected

--- VulkanProject/main.cpp
+++ VulkanProject/main.cpp
@@ -1,2 +1,27 @@
-#include <glm/glm.hpp>
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h> 
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
+
+int main() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Test Window", nullptr, nullptr);
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	printf("Extension count %d", extensionCount);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+}
